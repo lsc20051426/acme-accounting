b809e7232315e6c1490e32387ca52966dcdd7b93
Vitaly Korneev <korneev@osome.com>
2025-08-25 21:59:40 +0400

    Add requests delay

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,9 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type ProxyOptions } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'dev-api-delay',
+      configureServer(server) {
+        server.middlewares.use(async (req, _res, next) => {
+          if (req.url?.startsWith('/api/')) {
+            await new Promise((r) => setTimeout(r, 1000));
+          }
+          next();
+        });
+      },
+    },
+  ],
   server: {
     port: 5173,
     proxy: {
